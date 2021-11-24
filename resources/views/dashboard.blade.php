7eec3532a5b80df4e2f230acf77242e998f5c1b9
William Lima <williamlims@hotmail.com>
2021-11-23 22:03:13 -0300

    Criação das páginas Home e Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,5 +9,110 @@
 @endsection
 
 @section('conteudo')
-    Dashboard aqui ...
+    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+    <br>
+    <div class="row">
+        
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Custo do Estoque
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">R$40,000</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa fa-dollar fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Valor do Estoque
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">R$100,000</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa fa-dollar fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                Quantidade
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">45</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa fa-list-ol fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                Transferências
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">30</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa fa-exchange fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="mb-3 mx-2 col">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Itens mais caros</h6>
+                </div>
+                <div class="card-body">
+                    <h4 class="small font-weight-bold">PC DataCenter <span
+                            class="float-right">R$20,000</span></h4>
+                    <div class="progress mb-4">
+                        <div class="progress-bar bg-danger" role="progressbar" style="width: 50%"
+                            aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <h4 class="small font-weight-bold">Macbook Air <span
+                            class="float-right">R$10,000</span></h4>
+                    <div class="progress mb-4">
+                        <div class="progress-bar bg-warning" role="progressbar" style="width: 25%"
+                            aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <h4 class="small font-weight-bold">Computador Desktop XPR 300 <span
+                            class="float-right">R$10,000</span></h4>
+                    <div class="progress mb-4">
+                        <div class="progress-bar" role="progressbar" style="width: 25%"
+                            aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,5 +9,39 @@
 @endsection
 
 @section('conteudo')
-    Conteúdo aqui ...
+    <br>
+    <div class="row">
+        <div class="col-4">
+            <div class="card" >
+                <img src="{{ asset('img/itens-estoque.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Itens no Estoque</h5>
+                    <p class="card-text">Verifique os itens cadastrados no estoque e controle os ativos com as ferramentas de controle de estoque.</p>
+                    <a href="{{ url('/itens') }}" class="btn btn-primary">Listar Itens</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card" >
+                <img src="{{ asset('img/movimento.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Movimentações</h5>
+                    <p class="card-text">Verifique as movimentações de itens no estoque e controle os ativos com as ferramentas de controle de estoque.</p>
+                    <a href="{{ url('/movimentacoes') }}" class="btn btn-primary">Listar Movimentações</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card" >
+                <img src="{{ asset('img/cadastro.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Cadastrar Item</h5>
+                    <p class="card-text">Cadastre novos itens no estoque e controle os ativos com as ferramentas de controle de estoque.</p>
+                    <a href="{{ url('/item') }}" class="btn btn-primary">Cadastrar Novo Item</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
