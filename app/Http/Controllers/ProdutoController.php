769e29d150032109eec406723d9a19d1f05c484f
William Lima <williamlims@hotmail.com>
2021-12-03 00:13:53 -0300

    Criação das rotinas na Controller e view

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Produto;
 use App\Models\Deposito;
+use App\Models\Produto;
+use App\Models\Movimento;
+use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
@@ -32,10 +33,122 @@ class ProdutoController extends Controller
 
     public function saveItem(Request $request){
         $resultado = Produto::create($request->all());
+
+        $movimento = new Movimento;
+        $movimento->nome_estoque = $request->id_estoque;
+        $movimento->produto = $request->produto;
+        $movimento->tipo_transferencia = "ENTRADA";
+        $movimento->descricao = $request->descricao;
+        $movimento->lote = $request->lote;
+        $movimento->quantidade = $request->quantidade;
+
+        $resultado2 = $movimento->save();
         $depositos = Deposito::all();
-        if($resultado){
+
+        if($resultado && $resultado2){
             echo "<script> alert('Produto Cadastrado com Sucesso!'); </script>";
             return view('item', compact('depositos'));
         }
+    }
+
+    public function transferItem(Request $request){
+
+        $movimento_entrada = new Movimento;
+        $movimento_saida = new Movimento;
+
+        $produto = Produto::findOrFail($request->iditem);
+
+        if($request->transacao == "Saida") {
+
+            if($produto->quantidade >= $request->quantidade){
+
+                $movimento_saida->nome_estoque = $request->origem;
+                $movimento_saida->produto = $produto->produto;
+                $movimento_saida->tipo_transferencia = "SAÍDA";
+                $movimento_saida->descricao = $request->descricao;
+                $movimento_saida->lote = $produto->lote;
+                $movimento_saida->quantidade = $request->quantidade;
+
+                if($produto->quantidade == $request->quantidade){
+                    $res = $movimento_saida->save();
+                    $atualiza_prod = $produto->delete();
+                } else {
+                    $res = $movimento_saida->save();
+                    $produto->quantidade = $produto->quantidade - $request->quantidade;
+                    $atualiza_prod = $produto->save();
+                }
+                
+                if($res && $atualiza_prod){
+                    echo "<script> alert('Saída do estoque com sucesso!'); </script>";
+                    $movimentos = Movimento::all();
+                    return view('movimentacoes', compact('movimentos'));
+                }
+
+            } else {
+                echo "<script> alert('Quantidade acima do que existe no estoque!'); </script>";
+                $movimentos = Movimento::all();
+                return view('movimentacoes', compact('movimentos'));
+            }
+            
+        } else if($request->transacao == "Transferencia") {
+
+            if($produto->quantidade >= $request->quantidade){
+
+                $movimento_saida->nome_estoque = $request->origem;
+                $movimento_saida->produto = $produto->produto;
+                $movimento_saida->tipo_transferencia = "SAÍDA";
+                $movimento_saida->descricao = $request->descricao;
+                $movimento_saida->lote = $produto->lote;
+                $movimento_saida->quantidade = $request->quantidade;
+
+                $movimento_entrada->nome_estoque = $request->destino;
+                $movimento_entrada->produto = $produto->produto;
+                $movimento_entrada->tipo_transferencia = "ENTRADA";
+                $movimento_entrada->descricao = $request->descricao;
+                $movimento_entrada->lote = $produto->lote;
+                $movimento_entrada->quantidade = $request->quantidade;
+
+                if($produto->quantidade == $request->quantidade){
+                    $res_saida = $movimento_saida->save();
+                    $res_entrada = $movimento_entrada->save();
+                    $produto->id_deposito = $request->destino;
+                    $atualiza_prod = $produto->save();
+                } else {
+                    $res_saida = $movimento_saida->save();
+                    $res_entrada = $movimento_entrada->save();
+                    $produto->quantidade = $produto->quantidade - $request->quantidade;
+                    $atualiza_prod = $produto->save();
+                    $novo = new Produto;
+                    $novo->codigo = $produto->codigo;
+                    $novo->produto = $produto->produto;
+                    $novo->id_estoque = $request->destino;
+                    $novo->descricao = $request->descricao;
+                    $novo->lote = $produto->lote;
+                    $novo->preco = $produto->preco;
+                    $novo->custo = $produto->custo;
+                    $novo->quantidade = $request->quantidade;
+                    $novo->status = $produto->status;
+
+                    $salva_prod = $novo->save();
+                }
+
+                if($res_saida && $res_entrada){
+                    echo "<script> alert('Transferência efetuada com sucesso!'); </script>";
+                    $movimentos = Movimento::all();
+                    return view('movimentacoes', compact('movimentos'));
+                }
+
+            } else {
+                echo "<script> alert('Quantidade acima do que existe no estoque!'); </script>";
+                $movimentos = Movimento::all();
+                return view('movimentacoes', compact('movimentos'));
+            }
+
+        } else {
+            echo "<script> alert('Escolha uma opção de transferência!'); </script>";
+            $depositos = Deposito::all();
+            return view('transferencia', compact('depositos'));
+        }
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\DepositoController;
 use App\Http\Controllers\ProdutoController;
 
 use App\Models\Deposito;
+use App\Models\Produto;
+use App\Models\Movimento;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,7 +23,51 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $custo = number_format(Produto::sum('custo'),2,",",".");
+    $preco = number_format(Produto::sum('preco'),2,",",".");
+    $quant = Produto::sum('quantidade');
+    $moviments = Movimento::count('id');
+
+    $itemMax = Produto::where('status', 1)->orderByDesc('custo')->limit(3)->get();
+
+    $linhas = count($itemMax);
+
+    if($linhas == 3){
+
+        $total = $itemMax[0]->custo + $itemMax[1]->custo + $itemMax[2]->custo;
+        $v1 = ($itemMax[0]->custo / $total) * 100;
+        $v2 = ($itemMax[1]->custo / $total) * 100;
+        $v3 = ($itemMax[2]->custo / $total) * 100;
+        $percentual[0] = $v1;
+        $percentual[1] = $v2;
+        $percentual[2] = $v3;
+
+    } else if ($linhas == 2){
+
+        $total = $itemMax[0]->custo + $itemMax[1]->custo;
+        $v1 = ($itemMax[0]->custo / $total) * 100;
+        $v2 = ($itemMax[1]->custo / $total) * 100;
+        $v3 = 0;
+        $percentual[0] = $v1;
+        $percentual[1] = $v2;
+        $percentual[2] = $v3;
+
+    } else if ($linhas == 1){
+        $v1 =  100;
+        $v2 = 0;
+        $v3 = 0;
+        $percentual[0] = $v1;
+        $percentual[1] = $v2;
+        $percentual[2] = $v3;
+
+    } else {
+        $percentual[0] = 0;
+        $percentual[1] = 0;
+        $percentual[2] = 0;
+        $itemMax = null;
+    }
+
+    return view('dashboard', compact('custo', 'preco', 'quant', 'moviments', 'itemMax', 'percentual'));
 });
 
 Route::get('/deposito', function () {
@@ -34,17 +80,22 @@ Route::get('/item', function () {
 });
 
 Route::get('/itens', function () {
-    return view('itens');
-});
-
-Route::get('/movimentacoes', function () {
-    return view('movimentacoes');
+    $produtos = Produto::all();
+    return view('itens', compact('produtos'));
 });
 
 Route::get('/transferencia', function () {
-    return view('transferencia');
+    $depositos = Deposito::all();
+    return view('transferencia', compact('depositos'));
+});
+
+Route::get('/movimentacoes', function () {
+    $movimentos = Movimento::all();
+    return view('movimentacoes', compact('movimentos'));
 });
 
 Route::post('savedepform', [DepositoController::class, 'saveForm']);
 
 Route::post('saveitemform', [ProdutoController::class, 'saveItem']);
+
+Route::post('transferir', [ProdutoController::class, 'transferItem']);
